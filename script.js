750a7fde6b7294449660519fd7bca0501bb364ae
Ewa Szczepska <ewapietrek@outlook.com>
2023-01-23 13:40:40 +0300

    Styled display for desktop

--- script.js
+++
@@ -1,7 +0,0 @@
-var lastHeight = 0;
-$('.bg-portrait').each(function() {
-  $(this).css('background-position', '0 -' + lastHeight + 'px');
-  var currentHeight = $(this).outerHeight();
-  var newPosition = currentHeight + lastHeight;
-  lastHeight = lastHeight + currentHeight;
-});
